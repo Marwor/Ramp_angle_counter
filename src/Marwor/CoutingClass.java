313b31247d7e7ddeb448cbdf0f31807b8b567022
Marwor <borowkam@gmail.com>
2017-08-05 11:22:48 +0200

    exeption counter bug fix

--- src/Marwor/CoutingClass.java
+++ src/Marwor/CoutingClass.java
@@ -63,9 +63,11 @@ public class CoutingClass {
     }
     public boolean EnoughData(){
         if(ExceptionCounter>2){
+            ExceptionCounter=0;
             return false;
         }
         else{
+            ExceptionCounter=0;
             return true;
         }
     }
